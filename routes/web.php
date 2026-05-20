44cca92ada7e8da14b9468e650157dca8a756699
gileduardo <gilgea@gmail.com>
2026-05-20 11:22:31 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/curso', CursoController::class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/curso/teste', [CursoController::class, 'index']);
+
+
+// Route::resource('/curso', CursoController::class);
+
+
